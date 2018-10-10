c128dbe6d6db2bc02c1f5b53c063dffa35521571
sbendell <43750640+sbendell@users.noreply.github.com>
2018-10-10 13:51:14 +0100

    Created MyStack class
    Declared constrtuctor and destructor

---
+++ Lesson 11 - Templates/MyStack.h
@@ -0,0 +1,20 @@
+#pragma once
+
+template <typename T>
+class MyStack
+{
+public:
+	MyStack();
+	~MyStack();
+};
+
+template <typename T>
+MyStack<T>::MyStack() {
+
+}
+
+template <typename T>
+MyStack<T>::~MyStack() {
+
+}
+
