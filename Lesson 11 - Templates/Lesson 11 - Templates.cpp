a1c1c9617ce3d0eaf3bc0ec36c66bb92f23d1bce
sbendell <43750640+sbendell@users.noreply.github.com>
2018-10-10 13:18:17 +0100

    Copied code/created char matrix

--- Lesson 11 - Templates/Lesson 11 - Templates.cpp
+++ Lesson 11 - Templates/Lesson 11 - Templates.cpp
@@ -2,10 +2,34 @@
 //
 
 #include "stdafx.h"
+#include <iostream>
+#include "Matrix.h"
 
+using namespace std;
 
 int main()
 {
+	Matrix<char> charMat(3, 3);
+
+	for (int y = 0; y < charMat.get_y_size(); y++)
+	{
+		for (int x = 0; x < charMat.get_x_size(); x++)
+		{
+			charMat.set_element(x, y, char(97 + x + (y * 3)));
+		}
+	}
+
+	for (int y = 0; y < charMat.get_y_size(); y++)
+	{
+		for (int x = 0; x < charMat.get_x_size(); x++)
+		{
+			cout << charMat.get_element(x, y);
+		}
+	}
+
+	int x;
+	cin >> x;
+
     return 0;
 }
 
