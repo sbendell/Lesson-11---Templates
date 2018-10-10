4830bfc4ba78d8f05b7835554262c0bb4a66ea6c
sbendell <43750640+sbendell@users.noreply.github.com>
2018-10-10 15:15:40 +0100

    Stashed size value and fixed errors

--- Lesson 11 - Templates/MyStack.h
+++ Lesson 11 - Templates/MyStack.h
@@ -7,15 +7,18 @@ public:
 	MyStack();
 	~MyStack();
 	void Push(T item);
-	void Pop();
+	T Pop();
 	int Size();
 	bool Empty();
 private:
-	T** values;
+	T* values;
+	int size;
 };
 
 template <typename T>
-MyStack<T>::MyStack() {
+MyStack<T>::MyStack():
+	size(0) {
+	values = new T[size];
 }
 
 template <typename T>
@@ -25,33 +28,34 @@ MyStack<T>::~MyStack() {
 
 template <typename T>
 void MyStack<T>::Push(T item) {
-	T** newValues = new T*[Size() + 1];
+	int newArraySize = ++size;
+	T* newValues = new T[newArraySize];
 
-	for (int i = 0; i < arraySize; i++)
+	for (int i = 0; i < Size(); i++)
 	{
 		newValues[i] = values[i];
-		delete values[i];
 	}
-	delete values;
 
-	newValues[Size() - 1] = new T*(item);
+	delete[] values;
+	newValues[newArraySize - 1] = item;
 
 	values = newValues;
 }
 
 template <typename T>
-void MyStack<T>::Pop() {
-	delete values[Size() - 1];
+T MyStack<T>::Pop() {
+	size--;
+	return values[Size()];
 }
 
 template <typename T>
 int MyStack<T>::Size() {
-	return sizeof(values) / sizeof(T);
+	return size;
 }
 
 template <typename T>
 bool MyStack<T>::Empty() {
-	if (sizeof(values) == 0)
+	if (Size == 0)
 		return true;
 	else
 		return false;
