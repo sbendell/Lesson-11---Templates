cef9558bab6b000a0fb13e54337c54f7ab737c35
sbendell <43750640+sbendell@users.noreply.github.com>
2018-10-10 14:01:01 +0100

    Created empty methods

--- Lesson 11 - Templates/MyStack.h
+++ Lesson 11 - Templates/MyStack.h
@@ -6,15 +6,40 @@ class MyStack
 public:
 	MyStack();
 	~MyStack();
+	void Push(T item);
+	void Pop();
+	int Size();
+	bool Empty();
+private:
+	T** values;
 };
 
 template <typename T>
 MyStack<T>::MyStack() {
-
 }
 
 template <typename T>
 MyStack<T>::~MyStack() {
+
+}
+
+template <typename T>
+void MyStack<T>::Push(T item) {
+
+}
+
+template <typename T>
+void MyStack<T>::Pop() {
+
+}
+
+template <typename T>
+int MyStack<T>::Size() {
+
+}
+
+template <typename T>
+bool MyStack<T>::Empty() {
 
 }
 
